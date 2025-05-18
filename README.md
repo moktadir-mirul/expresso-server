215277d088937de226c31a627dfcdfbf9bdfb4a2
moktadir-mirul <moktadir.mirul@gmail.com>
2025-05-18 08:15:15 +0600

    server initialized

---
+++ README.md
@@ -0,0 +1,10 @@
+# Coffe-Store Server
+
+- A server based on Express, Mongo DB.
+
+***Author-Mirul Moktadir Khan***
+
+### Package used-
+#### MongoDB
+#### Express JS
+#### DOT ENV
---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+
+const uri = `mongodb+srv://mirul-moktadir:${process.env.DB_PASSWORD}@cluster0.mnwmrsu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+app.use(cors());
+app.use(express.json());
+
+async function run () {
+    try {
+        await client.connect();
+        await client.db('admin').command({ping : 1})
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
+
+app.listen(port, () => {
+    console.log(`Coffee store server is running: ${port}`)
+})
