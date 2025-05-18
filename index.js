0ee6d3018aa54fb00948b71b7aad87c9e10c5d35
moktadir-mirul <moktadir.mirul@gmail.com>
2025-05-18 23:55:51 +0600

    delete method working

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const uri = `mongodb+srv://mirul-moktadir:${process.env.DB_PASSWORD}@cluster0.mnwmrsu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -35,6 +35,14 @@ async function run () {
             const result = await coffeeCollection.insertOne(doc);
             console.log(result)
             res.send(result)
+        })
+
+        app.delete("/coffees/:id", async(req, res) => {
+          const id = req.params.id;
+          const query = {_id: new ObjectId(id)};
+          const result = await coffeeCollection.deleteOne(query);
+          console.log(result);
+          res.send(result);
         })
 
         await client.db('admin').command({ping : 1})
