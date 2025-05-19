62f55f23e50c20da6422125d69b97e027e3fc51c
moktadir-mirul <moktadir.mirul@gmail.com>
2025-05-19 07:04:29 +0600

    get method for a single item done

--- index.js
+++ index.js
@@ -30,6 +30,14 @@ async function run () {
           res.send(result);
         })
 
+        app.get("/coffees/:id", async(req, res) => {
+          const id = req.params.id;
+          const query = {_id: new ObjectId(id)};
+          const result = await coffeeCollection.findOne(query);
+          res.send(result);
+          console.log(result);
+        })
+
         app.post("/coffees", async(req, res) => {
             const doc = req.body;
             const result = await coffeeCollection.insertOne(doc);
